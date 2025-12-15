cfc384d8a9047ac2a2aaf47b9d6de52dafa57717
Vladimir <vladimir.ivanov1307@gmail.com>
2025-12-15 17:09:28 +0300

    fix tablename datetime and sql bool

--- app/db/models/tasks.py
+++ app/db/models/tasks.py
@@ -1,6 +1,8 @@
+from datetime import datetime
+
 from typing import TYPE_CHECKING
 
-from sqlalchemy import DateTime, ForeignKey
+from sqlalchemy import ForeignKey, false
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 
@@ -11,15 +13,15 @@ if TYPE_CHECKING:
 
 
 class Tasks(Base):
-    __name__ = "tasks"
+    __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str]
     description: Mapped[str | None]
-    is_done: Mapped[bool] = mapped_column(default=False, server_default=False, index=True)
-    created_at: Mapped[DateTime] = mapped_column(default=func.now(), server_default=func.now())
-    updated_at: Mapped[DateTime] = mapped_column(
-        default=func.now(),
+    is_done: Mapped[bool] = mapped_column(default=False, server_default=false(), index=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now(), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.now(),
         server_default=func.now(),
         onupdate=func.now,
     )
