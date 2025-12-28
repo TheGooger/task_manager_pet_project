c64036d5681ac9e0fe608e457b1b0433a62caa5d
vladimir <vladimir.ivanov1307@gmail.com>
2025-12-28 19:41:25 +0300

    add dep get_cuurent_user

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,1 +1,36 @@
+from typing import Optional
+
 from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import jwt, JWTError
+from sqlalchemy.orm import Session
+
+from app.db.session import get_session
+from app.db.models.users import Users
+from app.core.security import SECRET_KEY, ALGORITHM
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(
+        token: str = Depends(oauth2_scheme),
+        db: Session = Depends(get_session),
+) -> Users:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: Optional[str] = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    user = db.get(Users, int(user_id))
+
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+    if not user.is_active:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+
+    return user
