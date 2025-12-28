514044af43ce04d86552115a82ceb61fca8458fe
vladimir <vladimir.ivanov1307@gmail.com>
2025-12-28 19:41:07 +0300

    add users and auth routers

--- app/main.py
+++ app/main.py
@@ -2,9 +2,13 @@ from fastapi import FastAPI
 
 import uvicorn
 
+from app.api.routers import auth, users
+
 
 app = FastAPI()
 
+app.include_router(auth.router)
+app.include_router(users.router)
 
 @app.get("/")
 def hello_world():
