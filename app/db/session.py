a43367b8a3df193fbf586ecce629fca573a1bf5b
vladimir <vladimir.ivanov1307@gmail.com>
2026-01-04 23:54:31 +0500

    move to async and postgresql

--- app/db/session.py
+++ app/db/session.py
@@ -9,50 +9,25 @@ from .models import Base
 
 
 DATABASE_URL = get_db_url()
-DB_MODE = settings.DB_MODE
-
-
-# --- Sync setup ---
-if DB_MODE.startswith("sync"):
-    engine = create_engine(
-        DATABASE_URL,
-        echo=settings.ECHO,
-    )
-    SessionLocal = sessionmaker(
-        bind=engine,
-        expire_on_commit=False,
-        autoflush=False,
-        )
-    
-
-    def get_session() -> Generator[Session, None, None]:
-        """Sync dependency - yield DB Session"""
-        with SessionLocal() as session:
-            yield session
 
 
 # ---Async setup---
-elif DB_MODE.startswith("async"):
-    # create async engine 
-    async_engine = create_async_engine(
-        DATABASE_URL,
-        echo=settings.ECHO,
-        )
-    AsyncSessionLocal = async_sessionmaker(
-        bind=async_engine,
-        class_=AsyncSession,
-        expire_on_commit=False,
-        autoflush=False,
+# create async engine 
+async_engine = create_async_engine(
+    DATABASE_URL,
+    echo=settings.ECHO,
     )
+AsyncSessionLocal = async_sessionmaker(
+    bind=async_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False,
+)
 
-    async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
-        """Async dependency - yield AsyncSession"""
-        async with AsyncSessionLocal() as session:
-            yield session
-
-
-else:
-    raise RuntimeError(f"Unknown DB_MODE: {DB_MODE}")
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    """Async dependency - yield AsyncSession"""
+    async with AsyncSessionLocal() as session:
+        yield session
 
 
 async def init_db() -> None:
