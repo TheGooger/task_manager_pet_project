d019745fbd8e7952133682d4e2912f2ff1e60f32
vladimir <vladimir.ivanov1307@gmail.com>
2026-01-04 23:06:26 +0500

    re init alembic for postgres

--- alembic/env.py
+++ alembic/env.py
@@ -18,15 +18,14 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from app.db.models import Base
-from app.db.models import Users, Tasks
-from app.core.config import get_db_url
+from app.db.models.base import Base
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+from app.core.config import get_db_url
 config.set_main_option("sqlalchemy.url", get_db_url())
 
 
